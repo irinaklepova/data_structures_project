e2926ee5167ccc8445105de369effc7f44168de9
Irina Klepova <ivklepova23@gmail.com>
2023-09-30 17:26:35 +0400

    создан класс Stack на основе связанного списка

--- src/stack.py
+++ src/stack.py
@@ -1,13 +1,14 @@
 class Node:
     """Класс для узла стека"""
 
-    def __init__(self, data, next_node):
+    def __init__(self, data=None, next_node=None):
         """
         Конструктор класса Node
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Stack:
@@ -15,7 +16,8 @@ class Stack:
 
     def __init__(self):
         """Конструктор класса Stack"""
-        pass
+        self.top = None
+        self.size = 0
 
     def push(self, data):
         """
@@ -23,7 +25,11 @@ class Stack:
 
         :param data: данные, которые будут добавлены на вершину стека
         """
-        pass
+        new_node = Node(data, self.top)
+        if self.top:
+            new_node.next_node = self.top
+        self.top = new_node
+        self.size += 1
 
     def pop(self):
         """
@@ -31,4 +37,13 @@ class Stack:
 
         :return: данные удаленного элемента
         """
-        pass
+        if self.top is None:
+
+            return None
+        result = self.top.data
+        self.top = self.top.next_node
+        self.size -= 1
+        return result
+
+    def is_empty(self):
+        return self.top is None
