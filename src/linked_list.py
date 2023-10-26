e150ece133ecfc791fc04f3c60abf32694ea4d8d
Irina Klepova <ivklepova23@gmail.com>
2023-10-26 17:10:22 +0400

    реализованы классы `Node` и `LinkedList`,

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,31 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
 
 
 class LinkedList:
     """Класс для односвязного списка"""
 
+    def __init__(self):
+        self.head = None
+
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        self.head = Node(data, self.head)
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            node = self.head
+            while node.next_node is not None:
+                node = node.next_node
+            node.next_node = new_node
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
@@ -20,10 +33,10 @@ class LinkedList:
         if node is None:
             return str(None)
 
-        ll_string = ''
+        ll_string = ""
         while node:
-            ll_string += f' {str(node.data)} ->'
+            ll_string += f'{str(node.data)} -> '
             node = node.next_node
 
-        ll_string += ' None'
+        ll_string += 'None'
         return ll_string
