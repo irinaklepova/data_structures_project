cb0595a6160117e98fb493b88ff604ee205eb308
Irina Klepova <ivklepova23@gmail.com>
2023-10-27 01:19:25 +0400

    добавлены тесты для методов `to_list()` и `get_data_by_id()`

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -23,3 +23,39 @@ class TestLinkedList(unittest.TestCase):
         self.assertEqual(self.ll.head.next_node.data, {'id': 1})
         self.assertEqual(self.ll.head.next_node.next_node.data, {'id': 2})
         self.assertEqual(self.ll.head.next_node.next_node.next_node.data, {'id': 3})
+
+    def test_to_list(self):
+
+        self.ll = LinkedList()
+        self.ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        self.ll.insert_at_end({'id': 2, 'username': 'mik.roz'})
+        self.ll.insert_at_end({'id': 3, 'username': 'mosh_s'})
+        self.ll.insert_beginning({'id': 0, 'username': 'serebro'})
+        self.assertEqual(self.ll.to_list(), [{'id': 0, 'username': 'serebro'},
+                                                 {'id': 1, 'username': 'lazzy508509'},
+                                                 {'id': 2, 'username': 'mik.roz'},
+                                                 {'id': 3, 'username': 'mosh_s'}])
+
+        self.ll = LinkedList()
+        self.assertEqual(self.ll.to_list(), [])
+
+    def test_get_data_by_id(self):
+        self.ll = LinkedList()
+        # если список пуст
+        self.assertEqual(self.ll.get_data_by_id(3), None)
+        self.ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        self.ll.insert_at_end({'id': 2, 'username': 'mik.roz'})
+        self.ll.insert_at_end({'id': 3, 'username': 'mosh_s'})
+        self.ll.insert_beginning({'id': 0, 'username': 'serebro'})
+        # проверяем штатную работу
+        self.assertEqual(self.ll.get_data_by_id(2), {'id': 2, 'username': 'mik.roz'})
+
+        # проверка работы блока try/except
+        self.ll = LinkedList()
+        self.ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        self.ll.insert_at_end('idusername')
+        self.ll.insert_at_end([1, 2, 3])
+        self.ll.insert_at_end({'id': 2, 'username': 'mosh_s'})
+        self.assertEqual(self.ll.get_data_by_id(0),
+                         "TypeError: Данные не являются словарем или в словаре нет id.")
+
