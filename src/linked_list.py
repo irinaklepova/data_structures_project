43eea079862dfbd564f65608a25b3ba0e4d8eba0
Irina Klepova <ivklepova23@gmail.com>
2023-10-27 01:18:22 +0400

    добавлены в класс `LinkedList` методы `to_list()` и `get_data_by_id()`

--- src/linked_list.py
+++ src/linked_list.py
@@ -12,6 +12,20 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def __str__(self) -> str:
+        """Вывод данных односвязного списка в строковом представлении"""
+        node = self.head
+        if node is None:
+            return str(None)
+
+        ll_string = ""
+        while node:
+            ll_string += f'{str(node.data)} -> '
+            node = node.next_node
+
+        ll_string += 'None'
+        return ll_string
+
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
         self.head = Node(data, self.head)
@@ -27,16 +41,32 @@ class LinkedList:
                 node = node.next_node
             node.next_node = new_node
 
-    def __str__(self) -> str:
-        """Вывод данных односвязного списка в строковом представлении"""
+    def to_list(self):
+        """Возвращает список с данными, находящимися в односвязном списке"""
+        list_nodes = []
         node = self.head
         if node is None:
-            return str(None)
-
-        ll_string = ""
+            return list_nodes
         while node:
-            ll_string += f'{str(node.data)} -> '
+            list_nodes.append(node.data)
             node = node.next_node
+        return list_nodes
 
-        ll_string += 'None'
-        return ll_string
+    def get_data_by_id(self, id_value):
+        """Метод возвращает первый найденный в `LinkedList` словарь с ключом 'id',
+        значение которого равно переданному в метод значению"""
+        node = self.head
+        if node is None:
+            return None
+        while node:
+            try:
+                if node.data['id'] == id_value:
+                    return node.data
+                node = node.next_node
+
+            except TypeError:
+                node = node.next_node
+                return 'TypeError: Данные не являются словарем или в словаре нет id.'
+
+            if node is None:
+                break
